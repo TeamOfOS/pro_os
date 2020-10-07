1f4d393b2a3d64f7f4e658d9bf61e1ec70d54cd3
NonJetkGG <1136110248@qq.com>
2020-10-07 13:45:07 +0800

    2020-10-7

--- src/mananger/MemoryManager.java
+++ src/mananger/MemoryManager.java
@@ -1,12 +1,17 @@
 package mananger;
 
+import model.memory.Memory;
+import model.progress.PCB;
+import os.OS;
+
 public class MemoryManager {
+    private Memory memory = OS.memory;
 
-    public void allocateMemory() {
-
+    public void allocateMemory(byte[] program) throws Exception {
+        memory.allocate(program);
     }//分配内存的方法，参数未知
 
-    public void recoveredMemory() {
-
+    public void recoveredMemory(PCB pcb) {
+        memory.release(pcb);
     }//回收内存的方法，参数未知
 }
