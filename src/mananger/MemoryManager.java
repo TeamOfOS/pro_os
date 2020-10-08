d9d74fd548066e02c5dba2ba022f9097fb02b743
interestedgod <839395879@qq.com>
2020-10-09 01:27:01 +0800

    文件管理部分end！！!

--- src/mananger/MemoryManager.java
+++ src/mananger/MemoryManager.java
@@ -8,7 +8,7 @@ public class MemoryManager {
     private Memory memory = OS.memory;
 
     public void allocateMemory(byte[] program) throws Exception {
-        memory.allocate(program);
+       // memory.allocate(program);
     }//分配内存的方法，参数未知
 
     public void recoveredMemory(PCB pcb) {
