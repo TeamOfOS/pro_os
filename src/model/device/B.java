86428a1270df6db98ecd7a378c4bab5de676fddf
lyric-yu <1107028823@qq.com>
2020-10-15 11:03:51 +0800

    device-version1

---
+++ src/model/device/B.java
@@ -0,0 +1,8 @@
+package model.device;
+
+public class B extends Device {
+    public B(int count) {
+        super(count);
+        this.name="B";
+    }
+}
---
+++ src/model/device/C.java
@@ -0,0 +1,8 @@
+package model.device;
+
+public class C extends Device {
+    public C(int count) {
+        super(count);
+        this.name="C";
+    }
+}
